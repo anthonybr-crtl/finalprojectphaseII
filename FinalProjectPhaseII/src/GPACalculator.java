da51dd70678061adabd82c63a32cb918ad108adc
AlecMorganNSC <Alec.Morgan@seattlecolleges.edu>
2025-08-27 15:59:35 -0700

    Fixed a bug with GPA calculation (which was introduced in my refactor).

--- FinalProjectPhaseII/src/GPACalculator.java
+++ FinalProjectPhaseII/src/GPACalculator.java
@@ -5,7 +5,7 @@ public class GPACalculator {
 	// Calculates a student's GPA by dividing the total quality points 
 	public static double calculateGPA(Student student) {
 		double totalQualityPoints = calculateTQP(student.getNumberGrades(), student.getCourseCredits());
-		double totalCreditHours = calculateTCH(student.getNumberGrades());
+		double totalCreditHours = calculateTCH(student.getCourseCredits());
 		return totalQualityPoints / totalCreditHours;
 	}
 	
@@ -14,32 +14,10 @@ public class GPACalculator {
 		double TQP = 0;
 		for (int i = 0; i < numberGrades.size(); i++) {
 			double GPAP = 0;
-			if (numberGrades.get(i) >= 95) {
-				GPAP = 4.0;
-			} else if (numberGrades.get(i) >= 92) {
-				GPAP = 3.7;
-			} else if (numberGrades.get(i) >= 87) {
-				GPAP = 3.3;
-			} else if (numberGrades.get(i) >= 85) {
-				GPAP = 3.0;
-			} else if (numberGrades.get(i) >= 82) {
-				GPAP = 2.7;
-			} else if (numberGrades.get(i) >= 77) {
-				GPAP = 2.3;
-			} else if (numberGrades.get(i) >= 75) {
-				GPAP = 2.0;
-			} else if (numberGrades.get(i) >= 72) {
-				GPAP = 1.7;
-			} else if (numberGrades.get(i) >= 67) {
-				GPAP = 1.3;
-			} else if (numberGrades.get(i) >= 65) {
-				GPAP = 1.0;
-			} else if (numberGrades.get(i) >= 62) {
-				GPAP = 0.7;
-			} else {
-				GPAP = 0.0;
-			}
-			TQP = TQP + (GPAP * courseCredits.get(i));
+			double numberGrade = numberGrades.get(i);
+			GPAP = scaleToFour(numberGrade);
+			GPAP *= courseCredits.get(i); // Weight grade by credits. 
+			TQP += GPAP;
 		}
 		return TQP;
 	}
@@ -51,5 +29,18 @@ public class GPACalculator {
 			TCH += courseCredit;
 		}
 		return TCH;
+	}
+	
+	// Squishes a number grade from 0-100 to 0-4. 
+	public static double scaleToFour(double numberGrade) {
+		if (numberGrade > 100) {
+			System.out.println("Number grade must not be more than 100!");
+			return -1;
+		} else if (numberGrade < 0) {
+			System.out.println("Number grade must not be less than 0!");
+			return -1;
+		} else {
+			return numberGrade / 25; // Squish from 0-100 to 0-4.
+		}
 	}
 }
