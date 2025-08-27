421458b795dae0f3174619581d60a65d1fbc9cad
AlecMorganNSC <Alec.Morgan@seattlecolleges.edu>
2025-08-27 10:09:31 -0700

    Added error handling for too many/few credit hours.

--- FinalProjectPhaseII/src/Course.java
+++ FinalProjectPhaseII/src/Course.java
@@ -2,10 +2,38 @@ public class Course {
 
 	private double creditHours = 0;
 	private double numberGrade = 0;
+	private double minCreditHours = 1;
+	private double maxCreditHours = 5;
+	
+	// Set a Course's minimum credit hours.
+	public void setMinCreditHours(double min) {
+		this.minCreditHours = min;
+	}
+	
+	// Get a Course's minimum credit hours.
+	public double setMinCreditHours() {
+		return this.minCreditHours;
+	}
+	
+	// Set a Course's maximum credit hours.
+	public void setMaxCreditHours(double max) {
+		this.maxCreditHours = max;
+	}
+	
+	// Get a Course's maximum credit hours.
+	public double getMaxCreditHours() {
+		return this.maxCreditHours;
+	}
 	
 	// Set a Course's credit hours.
 	public void setCreditHours(double creditHours) {
-		this.creditHours = creditHours;
+		if (creditHours < minCreditHours) {
+			System.out.println("Credit hours must be at least " + minCreditHours + "!");
+		} else if (creditHours > maxCreditHours) {
+			System.out.println("Credit hours must be less than " + maxCreditHours + "!");
+		} else {
+			this.creditHours = creditHours;
+		}
 	}
 	
 	// Get a Course's credit hours.
--- FinalProjectPhaseII/src/UnitTester.java
+++ FinalProjectPhaseII/src/UnitTester.java
@@ -6,13 +6,34 @@ public class UnitTester {
 		GPACalculator gpacalc = new GPACalculator(); 
 		
 		try {
-			boolean pass;
-			course.setCreditHours(5.0);
-			pass = 5.0 == course.getCreditHours();
-			System.out.println("Set/Get credit hours works: " + pass);
+			// Run all unit test methods. 
+			testCreditHours(course);
+			testNumberGrade(course);
 		} catch (Exception e) {
 			System.out.println(e);
 		}
-		
+	}
+
+	// Test set/get credit hours methods.
+	public static void testCreditHours(Course course) {
+		// Test that we can set any value.
+		course.setCreditHours(5.0);
+		boolean pass = 5.0 == course.getCreditHours();
+		System.out.println("Can set/get credit hours: " + pass);
+		// Test if we can set a value that's too low. 
+		course.setCreditHours(-1.0);
+		pass = !(-1.0 == course.getCreditHours());
+		System.out.println("Can't set/get credit hours: " + pass);
+		// Test if we can set a value that's too high. 
+		course.setCreditHours(6.0);
+		pass = !(6.0 == course.getCreditHours());
+		System.out.println("Can't set/get credit hours: " + pass);
+	}
+
+	// Test set/get number grade methods.
+	public static void testNumberGrade(Course course) {
+		course.setNumberGrade(83);
+		boolean pass = 83 == course.getNumberGrade();
+		System.out.println("Set/Get credit hours works: " + pass);
 	}
 }
